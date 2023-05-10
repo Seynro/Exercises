e1166501e77e430cd4cd3a225ab4bf63568fdeb1
Seynro <oseymur0@gmail.com>
2023-05-10 13:41:51 +0400

    test1

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+print('Функция empty = ', np.empty(10, dtype='int16'))
+
+print('Функция eye = ', np.eye(10,10))
+
+print('Функция ones = ', np.ones((2,3,3)))
+
+print('Функция zeros = ', np.zeros((3,4,2)))
+
+print('Функция full = ', np.full((2,5,5), 12))
